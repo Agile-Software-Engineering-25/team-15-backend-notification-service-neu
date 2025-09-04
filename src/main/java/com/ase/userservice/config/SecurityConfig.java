0e32b03ed1e3b66a7f8fbc0b956934a5b4602fb0
Eco_Fresh_Kaese <alexander.jablo@gmail.com>
2025-09-04 10:32:50 +0200

    fixed linting :partying_face:

--- src/main/java/com/ase/userservice/config/SecurityConfig.java
+++ src/main/java/com/ase/userservice/config/SecurityConfig.java
@@ -1,9 +1,6 @@
 package com.ase.userservice.config;
 
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.lang.NonNull;
@@ -11,8 +8,12 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.filter.OncePerRequestFilter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
-import java.io.IOException;
+
 
 @Configuration
 public class SecurityConfig {
