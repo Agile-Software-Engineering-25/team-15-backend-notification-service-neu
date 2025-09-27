733839fb7e0e33fbd87eff7e9187e054fcac3e42
Eco_Fresh_Kaese <alexander.jablo@gmail.com>
2025-09-27 16:26:50 +0200

    move userId into requestparam

--- src/main/java/com/ase/notificationservice/controllers/NotificationController.java
+++ src/main/java/com/ase/notificationservice/controllers/NotificationController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.ase.notificationservice.dtos.NotificationCreationDto;
 import com.ase.notificationservice.dtos.UserDto;
@@ -67,9 +68,9 @@ public class NotificationController {
 
   @GetMapping
   public ResponseEntity<List<Notification>> getNotifications(
-      @RequestBody UserDto user) {
+      @RequestParam String userId) {
     List<Notification> notifications
-        = notificationService.getNotificationsForUser(user.getId());
+        = notificationService.getNotificationsForUser(userId);
     return ResponseEntity.ok(notifications);
   }
 
