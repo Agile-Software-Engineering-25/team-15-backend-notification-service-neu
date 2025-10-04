ab1474ad8c5297305724ec261220c3376bb3b196
Eco_Fresh_Kaese <alexander.jablo@gmail.com>
2025-10-04 20:20:32 +0200

    fix linting

--- src/main/java/com/ase/notificationservice/entities/Notification.java
+++ src/main/java/com/ase/notificationservice/entities/Notification.java
@@ -1,20 +1,19 @@
 package com.ase.notificationservice.entities;
 
 import java.time.Instant;
-import com.ase.notificationservice.enums.NotificationType;
-import com.ase.notificationservice.enums.NotifyType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import com.ase.notificationservice.enums.NotificationType;
+import com.ase.notificationservice.enums.NotifyType;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 import lombok.experimental.SuperBuilder;
-
 /**
  * Entity class representing a notification in the system.
  */
