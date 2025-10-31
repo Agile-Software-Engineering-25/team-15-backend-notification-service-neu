ad20ae3b5723c96bcff68c7b463bf93b54bc50c8
Eco_Fresh_Kaese <alexander.jablo@gmail.com>
2025-10-31 15:53:37 +0100

    fix linting

--- src/main/java/com/ase/notificationservice/security/QueryParameterBearerTokenResolver.java
+++ src/main/java/com/ase/notificationservice/security/QueryParameterBearerTokenResolver.java
@@ -7,6 +7,7 @@ import org.springframework.util.StringUtils;
 public class QueryParameterBearerTokenResolver implements BearerTokenResolver {
 
   public static final String TOKEN_PARAM = "auth_token";
+  public static final int BEARER_TOKEN_PREFIX_LENGTH = 7;
 
   @Override
   public String resolve(HttpServletRequest request) {
@@ -26,7 +27,7 @@ public class QueryParameterBearerTokenResolver implements BearerTokenResolver {
   private String resolveFromAuthorizationHeader(HttpServletRequest request) {
     String authHeader = request.getHeader("Authorization");
     if (StringUtils.hasText(authHeader) && authHeader.startsWith("Bearer ")) {
-      return authHeader.substring(7);
+      return authHeader.substring(BEARER_TOKEN_PREFIX_LENGTH);
     }
     return null;
   }
